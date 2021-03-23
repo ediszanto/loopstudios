49a8404a1521f85fbacbd172d18b4f60c7d89edb
edi <ediiszanto@gmail.com>
2021-03-23 10:18:16 +0200

    cards on desktop are looking fine

--- app/js/app.js
+++ app/js/app.js
@@ -3,18 +3,21 @@ const btnHamburger =document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const bannerImage =document.querySelector('.banner__image')
 const fadeElemems = document.querySelectorAll('.has-fade');
+inages
 
 btnHamburger.addEventListener('click', () => {
     if(!header.classList.contains('open')){
         header.classList.add('open');
         fadeElemems.forEach(element => {
-            console.log(element);
             element.classList.remove('has-fade');
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
         });     
     }else{
         header.classList.remove('open');
         fadeElemems.forEach(element => {
-
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
             element.classList.add('has-fade');
         }); 
        
